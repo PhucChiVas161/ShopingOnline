496d24a9d54f002578945270816d4a7e6ac18239
PhucChiVas161 <78865616+PhucChiVas161@users.noreply.github.com>
2023-07-14 08:14:44 +0700

    Test

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: "clustersgp.idgevsm.mongodb.net",
+  DB_USER: "PhucDataBase",
+  DB_PASS: "PhucDataBase",
+  DB_DATABASE: "shoppingonline",
+  EMAIL_USER: "12a8.vinhloc@gmail.com", // Microsoft mail service
+  EMAIL_PASS: "gzpzspmmxoguuudg",
+  JWT_SECRET: "abcxyz123",
+  JWT_EXPIRES: "60000000", // in milliseconds
+};
+module.exports = MyConstants;
